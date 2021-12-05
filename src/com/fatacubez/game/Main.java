b66b1898670de8ce258473c3677170057afc5d0f
Egor251 <egorpyshny@gmail.com>
2021-12-05 11:18:52 +0300

    Create game window

---
+++ src/com/fatacubez/game/Main.java
@@ -0,0 +1,20 @@
+package com.fatacubez.game;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Game game = new Game();
+
+    JFrame window = new JFrame("2048");
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setResizable(false);
+    window.add(game);
+    window.pack();
+    window.setLocationRelativeTo(null);
+    window.setVisible(true);
+
+    game.start();
+    }
+}
